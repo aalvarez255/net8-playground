cae94432854b81049682c4ff74ce62696520cd61
Adrián Álvarez <adrian.alvarez@scrm.eu>
2023-10-13 13:32:35 +0200

    fix sku

--- deploy/terraform/main.tf
+++ deploy/terraform/main.tf
@@ -7,8 +7,8 @@ resource "azurerm_log_analytics_workspace" "analytics_ws" {
   name                = "analytics-ws-001"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  sku                 = "Free"
-  retention_in_days   = 30
+  sku                 = "PerGB2018"
+  retention_in_days   = 7
 }
 
 resource "azurerm_container_app_environment" "app_env" {
